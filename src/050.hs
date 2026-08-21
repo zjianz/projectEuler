8be1401b9179e63c80d9390c40f774bb3d3c9e31
zjianz <1261582038@qq.com>
2026-08-21 13:44:36 +0800

    60

--- src/050.hs
+++ src/050.hs
@@ -16,21 +16,32 @@ time x = do
 limit :: Integer
 limit = 1000000
 
-walker n acc (p1:p2:ps)
-    | acc > limit = []
-    | isPrime acc = (walker (n+2) (acc+p1+p2) ps) ++ [n]
-    | otherwise   = walker (n+2) (acc+p1+p2) ps
+longest ::
+    Int ->         -- 最大长度
+    Int ->         -- 当前子串长度
+    Integer ->     -- 当前子串和
+    [Integer] ->   -- 后续质数序列
+    Int
+longest len index acc (p1:p2:ps)
+    | acc > limit = len
+    | isPrime acc = longest index (index+2) (acc+p1+p2) ps
+    | otherwise   = longest len (index+2) (acc+p1+p2) ps
 
+findbest :: 
+    [Integer] ->  -- 当前最佳子串
+    [Integer] ->  -- 滑动窗口
+    [Integer] ->  -- 后续质数序列
+    [Integer]
 findbest best group (p:restPrime)
     | acc > limit = best
-    | otherwise   = case walker 0 acc restPrime of
-        []    -> best
-        (n:_) -> let newbest = group ++ [p] ++ take n restPrime in
-                     findbest newbest (tail newbest) (drop n restPrime)
+    | otherwise   = case longest 0 0 acc restPrime of
+        0 -> best
+        n -> let newbest = group ++ [p] ++ take n restPrime in
+                 findbest newbest (tail newbest) (drop n restPrime)
     where acc = sum group + p
 
 result :: Integer
-result = sum $ findbest [] [] primes
+result = sum $ findbest [] [] (tail primes)
 
 ------------------------------------------------------------
 
